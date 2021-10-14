d5b8aaf14e67b7e061c0305eb1b18a7296c49d0a
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-10-14 10:52:28 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array,string){
+    return array.filter(givenItem => givenItem.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(array,string){
+    const fuzzyArray = array.filter(givenItem => givenItem.startsWith(string))
+    return fuzzyArray
+}
+
+
+function matchName(array,string){
+    const matchedArray = array.filter(givenDriver => givenDriver.name === string)
+    return matchedArray
+}
